47c866043b0e7cb8ba93d9ffd137906d2686842d
Jay Virtanen <jvirtanen@cloudbees.com>
2022-10-06 12:47:52 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,15 +1,14 @@
-# CjocSharedAgents
-Get the list of all shared agents and their status
+# DisableSharedAgents
+This script will disable all shared agents on a CJOC
 
-This is a python script to get the list and status of shared agents at CJOC level and then get drilldown to controller level.
 ## Prerequisite :
 ### Python3 with packages - json, sys, getpass, inquirer, requests
 
 ## Usage:
-Run the python script "ocSharedAgents.py" in this repo.
+Run the python script "disableSharedAgents.py" in this repo.
 ### python3 ocSharedAgents.py
 This will ask for user inputs such as :
 1) Enter jenkins url: example url - "http://example.jenkinshost/"
 2) Enter username:
-3) Password:
-4) What report you would like to see?: This is an option to be selected by up and down arrow key and then enter key
+3) API Key:
+4) A prompt to confirm the disable action
---
+++ disableSharedAgents.py
@@ -0,0 +1,44 @@
+from email import header
+from wsgiref import headers
+import requests
+import json
+import sys
+import getpass
+import inquirer
+from requests.auth import HTTPBasicAuth
+from requests import get
+from requests.exceptions import ConnectionError
+
+jenkins_url = input("Enter jenkins url:")
+username = input("Enter username:")
+apiToken = getpass.getpass(prompt='API Token:')
+questions = [inquirer.List('Action Type', message="What change would you like to carry out?",choices=['Nothing','Disable'])]
+answers = inquirer.prompt(questions)
+if (answers["Action Type"] == "Nothing"):
+    print("Doing nothing, exiting")
+    sys.exit()
+if not(jenkins_url.startswith("http://") or jenkins_url.startswith("https://")):
+    print ("Invalid url format")
+    sys.exit()
+try:
+    response = requests.get(jenkins_url+'/cjoc/view/all/api/json',auth=HTTPBasicAuth(username, apiToken), verify=False)
+    print(response.status_code)
+    if (response.status_code == 200 and answers["Action Type"] == "Disable"):
+        i=0
+        crumb = requests.get(jenkins_url+'/cjoc/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)',auth=HTTPBasicAuth(username, apiToken), verify=False)
+        jenkins_crumb= str(crumb.content).split(':')
+        jenkins_crumb[1] = jenkins_crumb[1][:-1]
+        header= {"Jenkins-Crumb": jenkins_crumb[1]}
+        for jobs in response.json()['jobs']:
+                if (response.json()['jobs'][i]['_class'] == "com.cloudbees.opscenter.server.model.SharedSlave"):
+                        AgentName =  (response.json()['jobs'][i]['name'])
+                        print("disabling " + AgentName)
+                        post = requests.post(jenkins_url + '/cjoc/job/' + AgentName + '/disable', headers= header, verify=False, auth=HTTPBasicAuth(username, apiToken))
+                        print(post.status_code)
+                i=i+1
+    else:
+        print ("Invalid credentials")
+        sys.exit()
+except ConnectionError:
+    print ("Invalid Url")
+

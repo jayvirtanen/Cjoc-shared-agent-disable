f06beeb321e8d3e0983424e02debf19c84ad0fdb
Jay Virtanen <jvirtanen@cloudbees.com>
2022-10-06 14:34:46 -0400

    added option for validation

--- README.md
+++ README.md
@@ -1,14 +1,22 @@
 # DisableSharedAgents
-This script will disable all shared agents on a CJOC
+
+This script does the following:
+1. Releases all leases on CJOC shared agents
+2. Disables all CJOC shared agents
 
 ## Prerequisite :
 ### Python3 with packages - json, sys, getpass, inquirer, requests
 
 ## Usage:
 Run the python script "disableSharedAgents.py" in this repo.
-### python3 ocSharedAgents.py
+
+### python3 disableSharedAgents.py
 This will ask for user inputs such as :
 1) Enter jenkins url: example url - "http://example.jenkinshost/"
 2) Enter username:
 3) API Key:
 4) A prompt to confirm the disable action
+
+## Options:
+--insecure
+Disables certificate validation on https requests
--- disableSharedAgents.py
+++ disableSharedAgents.py
@@ -9,7 +9,12 @@ import inquirer
 from requests.auth import HTTPBasicAuth
 from requests import get
 from requests.exceptions import ConnectionError
+import argparse
 
+parser = argparse.ArgumentParser()
+#Option for skipping Certificate validation
+parser.add_argument('--insecure', default=True, action="store_false")
+args= parser.parse_args()
 jenkins_url = input("Enter jenkins url:")
 username = input("Enter username:")
 apiToken = getpass.getpass(prompt='API Token:')
@@ -22,11 +27,11 @@ if not(jenkins_url.startswith("http://") or jenkins_url.startswith("https://")):
     print ("Invalid url format")
     sys.exit()
 try:
-    response = requests.get(jenkins_url+'/cjoc/view/all/api/json',auth=HTTPBasicAuth(username, apiToken), verify=False)
+    response = requests.get(jenkins_url+'/cjoc/view/all/api/json',auth=HTTPBasicAuth(username, apiToken), verify=args.insecure)
     print(response.status_code)
     if (response.status_code == 200 and answers["Action Type"] == "Disable"):
         i=0
-        crumb = requests.get(jenkins_url+'/cjoc/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)',auth=HTTPBasicAuth(username, apiToken), verify=False)
+        crumb = requests.get(jenkins_url+'/cjoc/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)',auth=HTTPBasicAuth(username, apiToken), verify=args.insecure)
         jenkins_crumb= str(crumb.content).split(':')
         jenkins_crumb[1] = jenkins_crumb[1][:-1]
         header= {"Jenkins-Crumb": jenkins_crumb[1]}
@@ -34,11 +39,11 @@ try:
                 if (response.json()['jobs'][i]['_class'] == "com.cloudbees.opscenter.server.model.SharedSlave"):
                         AgentName =  (response.json()['jobs'][i]['name'])
                         print("releasing " + AgentName)
-                        releasePost = requests.post(jenkins_url + '/cjoc/job/' + AgentName + '/doForceRelease', headers= header, auth=HTTPBasicAuth(username, apiToken), verify=False)
+                        releasePost = requests.post(jenkins_url + '/cjoc/job/' + AgentName + '/doForceRelease', headers= header, auth=HTTPBasicAuth(username, apiToken), verify=args.insecure)
                         if(releasePost.status_code == 200):
                             print(AgentName + " leases released successfully")
                         print("disabling " + AgentName)
-                        disablePost = requests.post(jenkins_url + '/cjoc/job/' + AgentName + '/disable', headers= header , auth=HTTPBasicAuth(username, apiToken), verify=False)
+                        disablePost = requests.post(jenkins_url + '/cjoc/job/' + AgentName + '/disable', headers= header , auth=HTTPBasicAuth(username, apiToken), verify=args.insecure)
                         if(disablePost.status_code == 200):
                             print(AgentName + " disabled successfully")
                 i=i+1
